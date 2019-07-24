4d6eb83e3d3b5a7b01acb7667dda0166c536b694
Yi Wang <yi.wang.2005@gmail.com>
2019-07-24 14:48:21 -0700

    Use tempfile

--- python/examples/synthesize_and_read.py
+++ python/examples/synthesize_and_read.py
@@ -7,6 +7,7 @@ import os
 import recordio
 import recordio
 import sys
+import tempfile
 import tensorflow as tf
 
 
@@ -77,27 +78,28 @@ def synthesize(args):
 
 
 def synthesize_and_read_data():
-    if not os.path.exists("/tmp/mnist"):
+    with tempfile.TemporaryDirectory() as dir:
         synthesize(argparse.Namespace(
-            dir='/tmp',
+            dir=dir,
             records_per_shard=16*1024,
             dataset="mnist",
             fraction=1.0))
 
-    tasks = [
-        ("/tmp/mnist/train/data-00000", 100, 256, 256),
-        ("/tmp/mnist/train/data-00001", 1024, 2048, 2048),
-        ("/tmp/mnist/train/data-00002", 0, -1, 16384),
-    ]
+        tasks = [
+            (os.path.join(dir, "mnist/train/data-00000"), 100, 256, 256),
+            (os.path.join(dir, "mnist/train/data-00001"), 1024, 2048, 2048),
+            (os.path.join(dir, "mnist/train/data-00002"), 0, -1, 16384),
+        ]
 
-    for t in tasks:
-        counts = 0
-        with closing(recordio.Scanner(t[0], t[1], t[2])) as reader:
-            r = reader.record()
-            while r:
-                counts += 1
+        for t in tasks:
+            counts = 0
+            with closing(recordio.Scanner(t[0], t[1], t[2])) as reader:
                 r = reader.record()
-            assert counts == t[3]
+                while r:
+                    counts += 1
+                    r = reader.record()
+                    print(counts, t[3])
+                    assert counts == t[3]
 
 
 if __name__ == "__main__":
